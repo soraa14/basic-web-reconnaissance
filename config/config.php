874b58e907a0331e6acbee1752d15bc558d23e27
Aziz <59786914+soraa14@users.noreply.github.com>
2022-08-10 18:35:51 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 # $base_url format must be (http://url.com or https://url.com)
 $base_url = 'http://localhost';
-$result_path = '/var/www/html/scan-results';
+$result_path = '/var/www/html/scan-results/';
 $wordlist_path = '/usr/share/seclists/Discovery/Web-Content/';
 ?>
