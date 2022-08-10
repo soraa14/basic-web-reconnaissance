179b7b5ff4b15f1b620b37956d8e906bf10562f6
Aziz <59786914+soraa14@users.noreply.github.com>
2022-08-10 19:32:38 +0700

    Update register.php

--- register.php
+++ register.php
@@ -13,11 +13,6 @@
   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="navbar-nav">
-    <div class="nav-item text-nowrap">
-      <a class="nav-link px-3" href="#">Sign out</a>
-    </div>
-  </div>
 </header>
     <br><br>
 
