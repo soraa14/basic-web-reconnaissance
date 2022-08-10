99b5293a00df9a668b51d95f3ceb0d9330815d87
Aziz <59786914+soraa14@users.noreply.github.com>
2022-08-10 15:59:41 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 # $base_url format must be (http://url.com or https://url.com)
-$base_url = 'https://cec4-180-244-129-190.ap.ngrok.io/basic-web-reconnaissance';
-$result_path = '/opt/lampp/htdocs/basic-web-reconnaissance/scan-results/';
+$base_url = 'http://localhost';
+$result_path = '/var/www/html/scan-results';
 $wordlist_path = '/usr/share/seclists/Discovery/Web-Content/';
 ?>
